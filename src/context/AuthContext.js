2160fc6eaf4de261b6f0eb9633020460c7c7be98
absol <rizwan.amir121@gmail.com>
2024-02-06 04:37:08 +0500

    resolved favicon issue

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,7 +4,7 @@ import React, { createContext, useContext, useState } from 'react';
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState("abi");
+  const [user, setUser] = useState(null);
   const setUsername = (input) => {
       setUser(input)
   }
